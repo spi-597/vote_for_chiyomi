59f2a0f53621c412602acc49559ee489e884dbb2
spi <go-man.spirit@live.jp>
2025-07-01 19:48:52 +0900

    対象曲非表示対応(突貫,カード修正1

--- src/js/const.js
+++ src/js/const.js
@@ -11,19 +11,19 @@ const COOKIE_LAST_VOTE_DATE = "lastVoteDate"
 // 楽曲リスト
 const songList = new Array(
     '夢追いの蝶'
-    , 'Butterfly Dream'
+    // , 'Butterfly Dream'
 );    
 
 // ポスト埋め込みURLリスト
 const mvEmbedList = new Array(
     '<iframe id="moveFrame" class="moveFrame" src="https://www.youtube.com/embed/YRXxEyReOm0?si=W4gRLRDR5IuY19aL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
-    ,'<iframe id="moveFrame" class="moveFrame" src="https://www.youtube.com/embed/rBXvnL-T8_s?si=5lcoV9ZijoP_lRki" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    // ,'<iframe id="moveFrame" class="moveFrame" src="https://www.youtube.com/embed/rBXvnL-T8_s?si=5lcoV9ZijoP_lRki" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
 );    
 
 // 埋め込みMV URLリスト
 const mvLinkList = new Array(
     'https://youtu.be/YRXxEyReOm0?si=fjvgWnrM1SRoM3if'
-    ,'https://youtu.be/rBXvnL-T8_s?si=fFETrNwx_Y2MX_dh'
+    // ,'https://youtu.be/rBXvnL-T8_s?si=fFETrNwx_Y2MX_dh'
 );    
 
 // ハッシュタグリスト
